62adce393693e20701e4345c3c416aec2ebe400b
Qian Hao <qi_an_hao@126.com>
2021-02-22 17:29:20 +0800

    import bob hash

--- bloom_filter/hash_chaining.cpp
+++ bloom_filter/hash_chaining.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lookup3.h"
 
 int main()
 {
-    printf("Hello world.\n");
+    const char* str = "Hello world.\n";
+    uint32_t hash_val = hashlittle(str, strlen(str), 0x01010101);
+    printf("%d\n", hash_val);
     return 0;
 }
---
+++ bloom_filter/lookup3.h
@@ -0,0 +1,7 @@
+#ifndef __INCLUDE_LOOKUP3_H__
+#define __INCLUDE_LOOKUP3_H__
+
+#include <stdint.h>
+uint32_t hashlittle(const void *key, size_t length, uint32_t initval);
+
+#endif
